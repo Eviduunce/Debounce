2ef92be038196bd0faa28517ef570f371cf77001
Eviduunce <timo@leisengang.online>
2025-10-07 08:39:07 +0200

    Initial Commit

---
+++ KCBforMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KCBforMac
+//
+//  Created by Timo Leisengang on 07.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KCBforMac/KCBforMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCBforMacApp.swift
+//  KCBforMac
+//
+//  Created by Timo Leisengang on 07.10.25.
+//
+
+import SwiftUI
+
+@main
+struct KCBforMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
